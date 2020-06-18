e15c7e78b2ed60e6a6f12803cbc4a767e9505163
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-06-17 23:21:02 -0400

    completed code and updated comments

--- script.js
+++ script.js
@@ -3,52 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // To create a new, secure password
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// GIVEN I need a new, secure password
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
-
-passwordText.value = password;
+  passwordText.value = password;
 }
-// generatePassword function should be below
-// function needs to go here
+// generatePassword function below
 function generatePassword() {
-  // THEN I am presented with a series of prompts for password criteria
-  // ?????? what series
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-  // ???? I thought that was a given
 
-  // collected all the data here
-  // ********* 1 prompt *************
-  // ********* need validation **************
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // we need a prompt here and ask for the length
+  //prompt for the length and turn string to number
   var passwordLength = prompt(
     "How long would you like your password? Pick a number between 8 and 128."
   );
   var confirmLength = parseInt(passwordLength);
   console.log(confirmLength);
-  // check IF the number is < 8 || > 128
+  
+  // length validation
   if (confirmLength < 8 || confirmLength > 128 || isNaN(confirmLength)) {
     alert("Incorrect number, please generate again!");
     return;
   }
 
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  // each prompt needs to be validated...
-  // in this case, we should probably use a function to turn the length into a number
+  // List of variables and array for the password criteria  
   var collectionValidChars = [];
   var abcCapsArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   var abcLowerArr = "abcdefghijklmnopqrstuvwxyz".split("");
   var numArr = "0123456789".split("");
   var specialArr = "!#$%&()*+,-./:;<=>?@^[\\]^_`{|}~".split("");
-  // ************* 4 confirms **************
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  // we need a confirm to ask the user if they want to use lowercase (push)
+
+  // confirm to ask the user if they want to use lowercase, for loop and push
   var addAbcLower = confirm(
     "Would you like your password to have lowercase letters?"
   );
@@ -57,8 +41,7 @@ function generatePassword() {
       collectionValidChars.push(abcLowerArr[i]);
     }
   }
-  console.log(collectionValidChars);
-  // we need a confirm to ask the user if they want to use uppercase (push)
+  // confirm to ask the user if they want to use uppercase, for loop and push to valid characters array
   var addAbcCaps = confirm(
     "Would you like your password to have uppercase letters?"
   );
@@ -67,16 +50,14 @@ function generatePassword() {
       collectionValidChars.push(abcCapsArr[i]);
     }
   }
-  console.log(collectionValidChars);
-  // we need a confirm to ask the user if they want to use numeric (push)
+  // confirm to ask the user if they want to use numeric, for loop and push to valid characters array
   var addNum = confirm("Would you like your password to have numeric letters?");
   if (addNum === true) {
     for (var i = 0; i < numArr.length; i++) {
       collectionValidChars.push(numArr[i]);
     }
   }
-  console.log(collectionValidChars);
-  // we need a confirm to ask the user if they want to use special characters (push)
+  // confirm to ask the user if they want to use special characters, for loop and push to valid characters array
   var addSpecial = confirm(
     "Would you like your password to have special characters?"
   );
@@ -85,30 +66,27 @@ function generatePassword() {
       collectionValidChars.push(specialArr[i]);
     }
   }
-  console.log(collectionValidChars);
-
-  // if the user has an empty collection of valid charecters alert
-  if(collectionValidChars.length === 0){
+  // alert if the user has an empty collection of valid charecters
+  if (collectionValidChars.length === 0) {
     alert("You must choose at least one Character Type! Please generate again");
     return;
   }
-
-  // end of collected all the data here************************
-  // using the number from above and the 4 booleans
-
-  // *********** need a function *****************
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // all the information is gathered (all prompts are finished getting input)
-  // all the HARD logic goes here ???????
-
-  // collectionValid [Math.floor(math.random()*passwordLength.length)]
+  
+  // using the specified length and the 4 criteria, create valid password using a for loop and variable for the password 
+  var randomPassword = "";
+  for (var i = 0; i < confirmLength; i++) {
+    collectionValidChars[
+      Math.floor(Math.random() * collectionValidChars.length)
+    ];
+    randomPassword +=
+      collectionValidChars[
+        Math.floor(Math.random() * collectionValidChars.length)
+      ];
+  }
+  return randomPassword;
 }
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
-// either do an alert with the password or just check to see if my variable has a password
-
 // Add event listener to generate button
 // WHEN I click the button to generate a password
 generateBtn.addEventListener("click", writePassword);
-// }
